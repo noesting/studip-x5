6dc2f0c4b2d0d9147cd21f3b682f645b5abda6d2
Nils Oesting <noesting@uni-osnabrueck.de>
2018-11-15 08:51:40 +0100

    added some lists

--- views/oer/index.php
+++ views/oer/index.php
@@ -8,73 +8,241 @@ if (isset($flash['message'])) {
 }
 ?>
 
-<p>
-    Es gibt im Moment in diese Mannschaft, oh, einige Spieler
-    vergessen ihren Profi was sie sind. Ich lese nicht sehr viele
-    Zeitungen, aber ich habe gehört viele Situationen.
-</p>
-<p>
-    Wir haben nicht offensiv gespielt. Es gibt keine deutsche
-    Mannschaft spielt offensiv und die Namen offensiv wie Bayern.
-    Letzte Spiel hatten wir in Platz drei Spitzen: Elber, Jancker und
-    dann Zickler. Wir mussen nicht vergessen Zickler. Zickler ist eine
-    Spitzen mehr, Mehmet mehr Basler.
-</p>
-<p>
-    Ist klar diese Wörter, ist möglich verstehen, was ich hab' gesagt?
-    Danke. Offensiv, offensiv ist wie machen in Platz.
-</p>
-<p>
-    Ich habe erklärt mit diese zwei Spieler: Nach Dortmund brauchen
-    vielleicht Halbzeit Pause. Ich habe auch andere Mannschaften
-    gesehen in Europa nach diese Mittwoch. Ich habe gesehen auch zwei
-    Tage die Training. Ein Trainer ist nicht ein Idiot! Ein Trainer
-    sehen was passieren in Platz. In diese Spiel es waren zwei, drei
-    oder vier Spieler, die waren schwach wie eine Flasche leer!
-</p>
-<p>
-    Haben Sie gesehen Mittwoch, welche Mannschaft hat gespielt
-    Mittwoch? Hat gespielt Mehmet, oder gespielt Basler, oder gespielt
-    Trapattoni? Diese Spieler beklagen mehr als sie spielen!
-</p>
-<p>
-    Wissen Sie, warum die Italien-Mannschaften kaufen nicht diese
-    Spieler? Weil wir haben gesehen viele Male solche Spiel. Haben
-    gesagt, sind nicht Spieler für die italienische Meisters.
-</p>
-<p>
-    Struuunz! Strunz ist zwei Jahre hier, hat gespielt zehn Spiele,
-    ist immer verletzt. Was erlauben Strunz?! Letzte Jahre Meister
-    geworden mit Hamann, eh Nerlinger. Diese Spieler waren Spieler und
-    waren Meister geworden. Ist immer verletzt! Hat gespielt 25 Spiele
-    in diese Mannschaft, in diese Verein. Muss respektieren die andere
-    Kollegen!
-</p>
-<p>
-    Haben viele Kollegen, stellen sie die Kollegen in Frage! Haben
-    keine Mut an Worten, aber ich weiß, was denken über diese Spieler.
-    Mussen zeigen jetzt, ich will, Samstag, diese Spieler mussen
-    zeigen mich, eh..., seine Fans, mussen alleine die Spiel gewinnen.
-    Muss allein die Spiel gewinnen!
-</p>
-<p>
-    Ich bin müde jetzt Vater diese Spieler, eh..., verteidige immer
-    diese Spieler. Ich habe immer die Schulde... über diese Spieler.
-    Einer ist Mario, ein anderer ist Mehmet. Strunz dagegen, egal, hat
-    nur gespielt 25 Prozent diese Spiele!
-</p>
-<p>
-    Ich habe fertig.
-</p>
+<div>
+    <?= \Studip\Button::create('Vorschläge', 'klickMichButton', array('data-dialog-button' => '1', 'data-hallo' => 'welt')); ?>
+    <?= \Studip\Button::create('Suche', 'klickMichButton', array('data-dialog-button' => '1', 'data-hallo' => 'welt')); ?>
+    <?= \Studip\Button::create('Meine Listen', 'klickMichButton', array('data-dialog-button' => '1', 'data-hallo' => 'welt')); ?>
+</div>
 
-<hr>
+<br>
 
-<?= \Studip\LinkButton::create(
+<h1>Meine Listen</h1>
+
+
+<div class="x5-default-list">
+    <h3>Meine Materialien</h3>
+
+    <div class="x5-default-list-item">
+        <div class="x5-item-thumbnail">
+        </div>
+
+        <div class="x5-item-inner">
+            Möbelindustrue 2030: Super krasse Möbel möblieren das ohnehin fantastische Mobiliar
+        </div>
+
+        <div class="x5-item-action">
+            <h3><?= new Icon('action') ?></h3>
+        </div>
+    </div>
+
+    <div class="x5-default-list-item">
+        <div class="x5-item-thumbnail">
+        </div>
+
+        <div class="x5-item-inner">
+            Lange Titel: Studien zeigen, dass ein äußerst langer Titel weder besonders einprägsam, noch in irgendeiner Weise Praktisch ist
+        </div>
+
+        <div class="x5-item-action">
+            <h3><?= new Icon('action') ?></h3>
+        </div>
+    </div>
+
+    <div class="x5-default-list-item">
+        <div class="x5-item-thumbnail">
+        </div>
+
+        <div class="x5-item-inner">
+        Es geht auch kurz
+        </div>
+
+        <div class="x5-item-action">
+            <h3><?= new Icon('action') ?></h3>
+        </div>
+    </div>
+
+    <div class="x5-default-list-item">
+        <div class="x5-item-thumbnail">
+        </div>
+
+        <div class="x5-item-inner">
+            Möbelindustrue 2030: Super krasse Möbel möblieren das ohnehin fantastische Mobiliar
+        </div>
+
+        <div class="x5-item-action">
+            <h3><?= new Icon('action') ?></h3>
+        </div>
+    </div>
+
+    <div class="x5-default-list-item">
+        <div class="x5-item-thumbnail">
+        </div>
+
+        <div class="x5-item-inner">
+            Möbelindustrue 2030: Super krasse Möbel möblieren das ohnehin fantastische Mobiliar
+        </div>
+
+        <div class="x5-item-action">
+            <h3><?= new Icon('action') ?></h3>
+        </div>
+    </div>
+
+    <div class="x5-default-list-item">
+        <div class="x5-item-thumbnail">
+        </div>
+
+        <div class="x5-item-inner">
+            Möbelindustrue 2030: Super krasse Möbel möblieren das ohnehin fantastische Mobiliar
+        </div>
+
+        <div class="x5-item-action">
+            <h3><?= new Icon('action') ?></h3>
+        </div>
+    </div>
+</div>
+
+
+<div class="x5-custom-list">
+    <h3>Titelsammlungen</h3>
+
+    <div class="x5-list-item">
+        <div class="x5-item-thumbnail">
+        </div>
+
+        <div class="x5-item-inner">
+            Möbelindustrue 2030: Super krasse Möbel möblieren das ohnehin fantastische Mobiliar
+        </div>
+
+        <div class="x5-item-action">
+            <h3><?= new Icon('action') ?></h3>
+        </div>
+    </div>
+
+    <div class="x5-list-item">
+        <div class="x5-item-thumbnail">
+        </div>
+
+        <div class="x5-item-inner">
+            Lange Titel: Studien zeigen, dass ein äußerst langer Titel weder besonders einprägsam, noch in irgendeiner Weise Praktisch ist
+        </div>
+
+        <div class="x5-item-action">
+            <h3><?= new Icon('action') ?></h3>
+        </div>
+    </div>
+
+    <div class="x5-list-item">
+        <div class="x5-item-thumbnail">
+        </div>
+
+        <div class="x5-item-inner">
+            Es geht auch kurz
+        </div>
+
+        <div class="x5-item-action">
+            <h3><?= new Icon('action') ?></h3>
+        </div>
+    </div>
+</div>
+
+<div class="x5-custom-list">
+    <h3>Kleine Sammlung</h3>
+
+    <div class="x5-list-item">
+        <div class="x5-item-thumbnail">
+        </div>
+
+        <div class="x5-item-inner">
+            Möbelindustrue 2030: Super krasse Möbel möblieren das ohnehin fantastische Mobiliar
+        </div>
+
+        <div class="x5-item-action">
+            <h3><?= new Icon('action') ?></h3>
+        </div>
+    </div>
+</div>
+
+<div class="x5-custom-list">
+    <h3>Andere Reihenfolge</h3>
+
+    <div class="x5-list-item">
+        <div class="x5-item-thumbnail">
+        </div>
+
+        <div class="x5-item-inner">
+            Es geht auch kurz
+        </div>
+
+        <div class="x5-item-action">
+            <h3><?= new Icon('action') ?></h3>
+        </div>
+    </div>
+
+    <div class="x5-list-item">
+        <div class="x5-item-thumbnail">
+        </div>
+
+        <div class="x5-item-inner">
+            Möbelindustrue 2030: Super krasse Möbel möblieren das ohnehin fantastische Mobiliar
+        </div>
+
+        <div class="x5-item-action">
+            <h3><?= new Icon('action') ?></h3>
+        </div>
+    </div>
+
+    <div class="x5-list-item">
+        <div class="x5-item-thumbnail">
+        </div>
+
+        <div class="x5-item-inner">
+            Lange Titel: Studien zeigen, dass ein äußerst langer Titel weder besonders einprägsam, noch in irgendeiner Weise Praktisch ist
+        </div>
+
+        <div class="x5-item-action">
+            <h3><?= new Icon('action') ?></h3>
+        </div>
+    </div>
+</div>
+
+<div class="x5-custom-list">
+    <h3>Nur lange Titel</h3>
+
+    <div class="x5-list-item">
+        <div class="x5-item-thumbnail">
+        </div>
+
+        <div class="x5-item-inner">
+            Lange Titel: Studien zeigen, dass ein äußerst langer Titel weder besonders einprägsam, noch in irgendeiner Weise Praktisch ist
+        </div>
+
+        <div class="x5-item-action">
+            <h3><?= new Icon('action') ?></h3>
+        </div>
+    </div>
+
+    <div class="x5-list-item">
+        <div class="x5-item-thumbnail">
+        </div>
+
+        <div class="x5-item-inner">
+            Möbelindustrue 2030: Super krasse Möbel möblieren das ohnehin fantastische Mobiliar
+        </div>
+
+        <div class="x5-item-action">
+            <h3><?= new Icon('action') ?></h3>
+        </div>
+    </div>
+</div>
+
+<!-- <hr> -->
+
+<!-- <?= \Studip\LinkButton::create(
     dgettext('x5', 'Foo starten'),
     $controller->url_for('oer/foo', 17)
-) ?>
+) ?> -->
 
-<hr>
+<!-- <hr>
 
 <form class="default" method="post" action="<?= $controller->url_for('oer/bar') ?>">
     <fieldset>
@@ -98,4 +266,4 @@ if (isset($flash['message'])) {
             $controller->url_for('oer/index')
         )?>
     </footer>
-</form>
+</form> -->
