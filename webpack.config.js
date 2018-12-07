9a13e37fcc4525d4ee84dd8d3ce5cd8ee44b5c58
Nils Oesting <noesting@uni-osnabrueck.de>
2018-12-07 08:50:43 +0100

    add vue to project

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,31 @@
-const path = require("path");
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
-  entry: "./src/index.js",
-  mode: "development",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist")
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          "style-loader", // creates style nodes from JS strings
-          "css-loader", // translates CSS into CommonJS
-          "sass-loader" // compiles Sass to CSS, using Node Sass by default
+    entry: './src/index.js',
+    mode: 'development',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader', // creates style nodes from JS strings
+                    'css-loader', // translates CSS into CommonJS
+                    'sass-loader' // compiles Sass to CSS, using Node Sass by default
+                ]
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }
         ]
-      }
-    ]
-  }
+    },
+    resolve: {
+        extensions: ['.vue', '.js']
+    },
+    plugins: [new VueLoaderPlugin()]
 };
