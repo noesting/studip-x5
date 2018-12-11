bb6e42ae0d3b202812748c6739dccf337a65b1da
Nils Oesting <noesting@uni-osnabrueck.de>
2018-12-11 09:22:48 +0100

    remove the no vue stuff

--- src/globals.js
+++
@@ -1,4 +0,0 @@
-export const LISTTYPES = {
-    CUSTOM: 'custom',
-    RECOMMENDATIONS: 'recommendations'
-};
--- src/index.js
+++ src/index.js
@@ -1,21 +1,7 @@
-import './x5_plugin.scss';
-
-import { X5ListController } from './controllers/list.controller';
-import { X5RecommendationsListController } from './controllers/recommendations-list.controller';
-import { X5CustomListController } from './controllers/custom-list.controller';
-
-import { LISTTYPES } from './globals';
-
 import App from './app/App.vue';
-
-export let recommendtionsListController;
-export let customListsController;
 
 const bootstrap = () => {
     document.addEventListener('DOMContentLoaded', function(event) {
-        initControllers();
-        addListeners();
-        startRender();
         startVue();
     });
 };
@@ -27,30 +13,5 @@ const startVue = () => {
         components: { App }
     });
 };
-
-const initControllers = () => {
-    recommendtionsListController = new X5RecommendationsListController();
-    customListsController = new X5CustomListController();
-};
-
-const startRender = () => {
-    recommendtionsListController.renderLists();
-};
-
-const addListeners = () => {
-    // $(document).on('click', "button[name='filterButton']", chooseFilterClick);
-    // $(document).on('click', "button[name='sortButton']", chooseSortClick);
-};
-
-const chooseFilterClick = event => {
-    console.log('click', 'chooseFilterClick');
-    console.log('event', event);
-};
-
-const chooseSortClick = () => {
-    console.log('click', 'chooseSortClick');
-};
-
-// ------------------------------------------------------------------------------------------------ START JS
 
 bootstrap();
