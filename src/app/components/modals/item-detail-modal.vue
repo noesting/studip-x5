e15148d18cd5ec0808957e31892fe931219cf1ac
Nils Oesting <noesting@uni-osnabrueck.de>
2018-12-18 09:20:22 +0100

    add more components to modal

--- src/app/components/modals/item-detail-modal.vue
+++ src/app/components/modals/item-detail-modal.vue
@@ -1,14 +1,23 @@
 <template>
     <div class="item-detail-container">
         <div class="title">{{ item.title }}</div>
+        <div class="subtitle">{{ item.publishingYear }} - {{ item.author }}</div>
         <div class="description">{{ item.description }}</div>
 
         <div class="description">{{ item.link }}</div>
 
         <div class="interaction-fields">
-            <div>Sichtbar für Studenten ab dem</div>
-            <div>Kommentarfeld</div>
-            <div>Bewertung</div>
+            <div class="interaction-grid-item">
+                Sichtbar für Studenten ab dem
+                <input value="dateString()" @click="pickDate">
+            </div>
+            <div class="interaction-grid-item">
+                Kommentarfeld
+                <textarea rows="4"></textarea>
+            </div>
+            <div class="interaction-grid-item">Bewertung
+                <StudipButton :text="'Bewerten'"></StudipButton>
+            </div>
         </div>
     </div>
 </template>
@@ -18,10 +27,27 @@
     import StudipIcon from '../studip-components/studip-icon-button-component.vue';
 
     export default {
-        components: {},
+        components: {
+            StudipButton
+        },
         props: ['item'],
-        computed: {},
-        methods: {}
+        computed: {
+            dateString() {
+                return (
+                    this.visibleDate.date() + '.' + (this.visibleDate.month() + 1) + '.' + this.visibleDate.currentYear()
+                );
+            }
+        },
+        data() {
+            return {
+                visibleDate: new Date()
+            };
+        },
+        methods: {
+            pickDate() {
+                console.log('picking date');
+            }
+        }
     };
 </script>
 
@@ -37,13 +63,22 @@
     .title {
         font-size: 1.5em;
         font-weight: bold;
-        margin-bottom: 1em;
+    }
+
+    .subtitle {
+        margin-bottom: 1.5em;
     }
 
     .interaction-fields {
         margin: 1em;
         display: grid;
         grid-template-columns: 33% 33% 33%;
+        grid-column-gap: 10px;
+    }
+
+    .interaction-grid-item {
+        padding: 1em;
+        background-color: #d4dbe5;
     }
 </style>
 
