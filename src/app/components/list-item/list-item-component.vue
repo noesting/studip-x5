6f014d84b579246224522be490833e2f18ec7bec
Nils Oesting <noesting@uni-osnabrueck.de>
2018-12-18 14:42:30 +0100

    add custom modal base

--- src/app/components/list-item/list-item-component.vue
+++ src/app/components/list-item/list-item-component.vue
@@ -15,7 +15,7 @@
         methods: {
             openModal() {
                 console.log('trying to open modal');
-                this.$modal.show(ItemDetailModal, { item: this.item }, { height: 'auto' });
+                this.$modal.show(ItemDetailModal, { item: this.item }, { height: 'auto', width: '70%' });
             }
         }
     };
--- src/app/components/modals/item-detail-modal.vue
+++ src/app/components/modals/item-detail-modal.vue
@@ -1,24 +1,32 @@
 <template>
     <div class="item-detail-container">
-        <div class="title">{{ item.title }}</div>
-        <div class="subtitle">{{ item.publishingYear }} - {{ item.author }}</div>
-        <div class="description">{{ item.description }}</div>
+        <div class="thumbnail-container"></div>
 
-        <div class="description">{{ item.link }}</div>
+        <div class="content">
+            <div class="title">{{ item.title }}</div>
+            <div class="subtitle">{{ item.publishingYear }} - {{ item.author }}</div>
+            <div class="description">{{ item.description }}</div>
 
-        <div class="interaction-fields">
-            <div class="interaction-grid-item">
-                Sichtbar für Studenten ab dem
-                <input value="dateString()" @click="pickDate">
+            <div class="link">
+                <a :href="itemLink" target="blank">{{ item.link }}</a>
             </div>
-            <div class="interaction-grid-item">
-                Kommentarfeld
-                <textarea rows="4"></textarea>
-            </div>
-            <div class="interaction-grid-item">Bewertung
-                <StudipButton :text="'Bewerten'"></StudipButton>
+
+            <div class="interaction-fields">
+                <div class="interaction-grid-item">
+                    Sichtbar für Studenten ab dem {{ dateString }}
+                    <StudipButton :text="'Datum auswählen'"></StudipButton>
+                </div>
+                <div class="interaction-grid-item">
+                    Kommentarfeld
+                    <textarea class="comment-text-area" rows="4"></textarea>
+                </div>
+                <div class="interaction-grid-item">Bewertung
+                    <StudipButton :text="'Bewerten'"></StudipButton>
+                </div>
             </div>
         </div>
+
+        <div class="right-placeholder"></div>
     </div>
 </template>
 
@@ -33,9 +41,11 @@
         props: ['item'],
         computed: {
             dateString() {
-                return (
-                    this.visibleDate.date() + '.' + (this.visibleDate.month() + 1) + '.' + this.visibleDate.currentYear()
-                );
+                return getDateInGermanFormat(this.visibleDate);
+            },
+
+            itemLink() {
+                return getValidLink(this.item.link);
             }
         },
         data() {
@@ -49,6 +59,23 @@
             }
         }
     };
+
+    const getDateInGermanFormat = date => {
+        const day = date.getDate() >= 10 ? date.getDate() : '0' + date.getDate();
+        const month = date.getMonth() >= 9 ? date.getMonth() + 1 : '0' + (date.getMonth() + 1);
+        const year = date.getFullYear();
+
+        return day + '.' + month + '.' + year;
+    };
+
+    const getValidLink = link => {
+        const includes = link.includes('http');
+        if (includes) {
+            return link;
+        }
+
+        return 'https://' + link;
+    };
 </script>
 
 <style lang="scss" scoped>
@@ -56,6 +83,9 @@
         padding: 1em;
         color: #1f3f70;
         background-color: #e7ebf1;
+
+        display: grid;
+        grid-template-columns: 15% 70% 15%;
 
         height: 100%;
     }
@@ -69,6 +99,11 @@
         margin-bottom: 1.5em;
     }
 
+    .link {
+        margin-top: 1.5em;
+        margin-bottom: 1.5em;
+    }
+
     .interaction-fields {
         margin: 1em;
         display: grid;
@@ -79,6 +114,14 @@
     .interaction-grid-item {
         padding: 1em;
         background-color: #d4dbe5;
+    }
+
+    .comment-text-area {
+        width: 100%;
+    }
+
+    input {
+        width: 100%;
     }
 </style>
 
