5329355d79f7b94a8bc87f3ceb85ae0c42752795
Nils Oesting <noesting@uni-osnabrueck.de>
2018-12-19 10:33:27 +0100

    added initial migration

---
+++ migrations/001_init_x5_plugin.php
@@ -0,0 +1,32 @@
+<?php
+
+class InitX5Plugin extends Migration
+{
+    public function up()
+    {
+        DBManager::get()->query("CREATE TABLE IF NOT EXISTS `x5_lists` (
+            `id` int(11) NOT NULL AUTO_INCREMENT,
+            `range_id` varchar(32) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
+            `position` int(10) unsigned NOT NULL,
+            `mkdate` int(11) NOT NULL DEFAULT '0',
+            `chdate` int(11) NOT NULL DEFAULT '0',
+            `visible` boolean NOT NULL,
+            PRIMARY KEY (`id`)
+        );");
+
+        DBManager::get()->query("CREATE TABLE IF NOT EXISTS `x5_list_items` (
+            `id` int(11) NOT NULL AUTO_INCREMENT,
+            `position` int(10) unsigned NOT NULL,
+            `mkdate` int(11) NOT NULL DEFAULT '0',
+            `chdate` int(11) NOT NULL DEFAULT '0',
+            `item_id` varchar(32) NOT NULL,
+            `list_id` int(11) NOT NULL,
+            PRIMARY KEY (`id`)
+        );");
+    }
+
+    public function down()
+    {
+        DBManager::get()->query('DROP TABLE IF EXISTS `x5_lists`, `x5_list_items`;');
+    }
+}
