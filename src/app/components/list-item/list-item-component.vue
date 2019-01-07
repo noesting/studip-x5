44cdc48c1cfbe957653bfee70823bd0fb6595c5f
Nils Oesting <noesting@uni-osnabrueck.de>
2019-01-07 14:58:30 +0100

    show more info in list item

--- src/app/components/list-item/list-item-component.vue
+++ src/app/components/list-item/list-item-component.vue
@@ -1,20 +1,31 @@
 <template>
     <div class="x5_list_item_inner" @click="openModal">
-        <div class="x5_list_item_thumbnail"></div>
+        <div class="x5_list_item_thumbnail">
+            <StudipIcon
+                class="x5-thumbnail-icon"
+                v-bind:icon_name="'file-' + item.type"
+                v-bind:color="'blue'"
+            ></StudipIcon>
+        </div>
         <div class="x5_list_item_title">{{ item.title }}</div>
-        <div
-            class="x5_list_item_subtitle"
-        >({{ item.thumbsUps }}) - {{ item.publishingYear }}: {{ item.author }}</div>
+        <div class="x5_list_item_subtitle">
+            <StudipIcon v-bind:icon_name="'smiley'" v-bind:color="'black'"></StudipIcon>
+            ({{ item.thumbsUps }}) - {{ item.publishingYear }}: {{ item.author }}
+        </div>
     </div>
 </template>
 
 <script>
     import ItemDetailModal from '../modals/item-detail-modal.vue';
+    import StudipIcon from '../studip-components/studip-icon-button-component.vue';
+
     export default {
         props: ['item'],
+        components: {
+            StudipIcon
+        },
         methods: {
             openModal() {
-                console.log('trying to open modal');
                 this.$modal.show(ItemDetailModal, { item: this.item }, { height: 'auto', width: '70%' });
             }
         }
@@ -53,6 +64,13 @@
         grid-row: 2;
 
         overflow: hidden;
+    }
+
+    .x5-thumbnail-icon {
+        margin-top: 0.5em;
+
+        height: 2em;
+        width: 2em;
     }
 </style>
 
