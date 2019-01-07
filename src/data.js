b660d7b8c233751d2174f33c58d1d91320b6041f
Nils Oesting <noesting@uni-osnabrueck.de>
2019-01-07 14:57:07 +0100

    altered data to fit our needs

--- src/data.js
+++ src/data.js
@@ -2,14 +2,15 @@ export const data = {
     recommendations: [
         {
             id: '0',
-            title: 'Möbelindustrie 2030: Super krasse Möbel möblieren das ohnehin fantastische Mobiliar',
+            title: 'Native Android Apps schreiben mit Java',
             description:
                 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
             author: 'Feyen, F.',
             publishingYear: '2016',
             link: 'www.google.de',
             thumbsUps: 23,
-            thumbnail: 'ein thumbnail'
+            thumbnail: 'ein thumbnail',
+            type: 'text'
         },
         {
             id: '1',
@@ -21,7 +22,8 @@ export const data = {
             publishingYear: '2016',
             link: 'www.google.de',
             thumbsUps: 2,
-            thumbnail: 'ein thumbnail'
+            thumbnail: 'ein thumbnail',
+            type: 'text'
         },
         {
             id: '2',
@@ -32,135 +34,159 @@ export const data = {
             publishingYear: '2018',
             link: 'www.google.de',
             thumbsUps: 230,
-            thumbnail: 'ein thumbnail'
+            thumbnail: 'ein thumbnail',
+            type: 'text'
         },
         {
             id: '3',
-            title: 'Die Arbeit eines Arbeiters',
+            title: 'Neuronale Netze',
             description:
                 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-            author: 'Autorium, A.',
+            author: 'Mustermann, M.',
             publishingYear: '1999',
             link: 'www.google.de',
             thumbsUps: 1,
-            thumbnail: 'ein thumbnail'
+            thumbnail: 'ein thumbnail',
+            type: 'pic'
         },
         {
             id: '4',
-            title: 'Die vegane Küche',
+            title: 'Introduction to Apache Cordova',
             description:
                 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
             author: 'Eibrink, K.',
             publishingYear: '1999',
             link: 'www.google.de',
             thumbsUps: 1,
-            thumbnail: 'ein thumbnail'
+            thumbnail: 'ein thumbnail',
+            type: 'text'
         },
         {
             id: '5',
-            title: 'Javascript: Eine unterschätzte Sprache',
+            title: 'NodeJS Tutorial',
             description:
                 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
             author: 'Lunzenauer, M.',
             publishingYear: '2005',
             link: 'www.google.de',
             thumbsUps: 1,
-            thumbnail: 'ein thumbnail'
+            thumbnail: 'ein thumbnail',
+            type: 'text'
         },
         {
             id: '6',
-            title: 'OpenCast',
+            title: 'OpenCast: Die Zukunft der Videoaufzeichnungen',
             description:
                 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
             author: 'Rolf, R.',
             publishingYear: '2008',
-            link: 'www.google.de',
+            link: 'https://opencast.org/',
             thumbsUps: 1,
-            thumbnail: 'ein thumbnail'
+            thumbnail: 'ein thumbnail',
+            type: 'text'
         },
         {
             id: '7',
-            title: 'Wie man Projekte an Land zieht',
+            title: 'Reguläre Ausdrücke',
             description:
                 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
             author: 'Thelen, T.',
             publishingYear: '2011',
             link: 'www.google.de',
             thumbsUps: 1,
-            thumbnail: 'ein thumbnail'
+            thumbnail: 'ein thumbnail',
+            type: 'video'
         },
         {
             id: '8',
-            title: 'Der lockere Führungsstil',
+            title: 'Projektmanagement - Ein Praxisleitfaden',
             description:
                 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
             author: 'Knaden, A.',
             publishingYear: '2016',
             link: 'www.google.de',
             thumbsUps: 1,
-            thumbnail: 'ein thumbnail'
+            thumbnail: 'ein thumbnail',
+            type: 'audio'
         }
     ],
     customLists: [
         {
-            title: 'Atomphysik',
+            title: 'Konzepte in der Informatik',
             list: [
                 {
                     id: '7',
-                    title: 'Wie man Projekte an Land zieht',
+                    title: 'Reguläre Ausdrücke',
                     description:
                         'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
                     author: 'Thelen, T.',
                     publishingYear: '2011',
                     link: 'www.google.de',
                     thumbsUps: 1,
-                    thumbnail: 'ein thumbnail'
+                    thumbnail: 'ein thumbnail',
+                    type: 'video'
                 },
                 {
-                    id: '8',
-                    title: 'Der lockere Führungsstil',
+                    id: '3',
+                    title: 'Neuronale Netze',
                     description:
                         'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-                    author: 'Knaden, A.',
-                    publishingYear: '2016',
+                    author: 'Mustermann, M.',
+                    publishingYear: '1999',
                     link: 'www.google.de',
                     thumbsUps: 1,
-                    thumbnail: 'ein thumbnail'
+                    thumbnail: 'ein thumbnail',
+                    type: 'pic'
                 }
             ],
             shared: false
         },
         {
-            title: 'Biochemie',
+            title: 'Apps',
             list: [
                 {
                     id: '4',
-                    title: 'Die vegane Küche',
+                    title: 'Introduction to Apache Cordova',
                     description:
                         'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
                     author: 'Eibrink, K.',
                     publishingYear: '1999',
                     link: 'www.google.de',
                     thumbsUps: 1,
-                    thumbnail: 'ein thumbnail'
+                    thumbnail: 'ein thumbnail',
+                    type: 'text'
                 },
                 {
-                    id: '5',
-                    title: 'Javascript: Eine unterschätzte Sprache',
+                    id: '0',
+                    title: 'Native Android Apps schreiben mit Java',
                     description:
                         'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-                    author: 'Lunzenauer, M.',
-                    publishingYear: '2005',
+                    author: 'Feyen, F.',
+                    publishingYear: '2016',
                     link: 'www.google.de',
-                    thumbsUps: 1,
-                    thumbnail: 'ein thumbnail'
+                    thumbsUps: 23,
+                    thumbnail: 'ein thumbnail',
+                    type: 'text'
                 }
             ],
             shared: true
         },
         {
-            title: 'Marcus Kurs für Kuriositäten',
-            list: [],
+            title: 'Physik',
+            list: [
+                {
+                    id: '2',
+                    title: 'Seismische Vibrationsmessung für Rasterkraftelektronenmikroskopie',
+                    description:
+                        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+                    author: 'Leißner, J.F.',
+                    publishingYear: '2018',
+                    link: 'www.google.de',
+                    thumbsUps: 230,
+                    thumbnail: 'ein thumbnail',
+                    type: 'text'
+                }
+            ],
             shared: true
         }
     ]
