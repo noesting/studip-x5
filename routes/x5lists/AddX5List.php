6830d53ab47bd9c7dd6b5dac700a407b6833f9db
Nils Oesting <noesting@uni-osnabrueck.de>
2019-01-11 09:19:19 +0100

    added some route setups

---
+++ routes/x5lists/AddX5List.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Argonauts\x5\Routes\Lists;
+
+use Argonauts\JsonApiController;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class AddX5List extends JsonApiController
+{
+    public function __invoke(Request $request, Response $response, $args)
+    {
+        // if (!$lists = X5List::find($args['id'])) {
+        //     throw new RecordNotFoundException();
+        // }
+
+        // if (1 == 2) {
+        //     throw new AuthorizationFailedException();
+        // }
+
+        // return $this->getContentResponse($lists);
+
+        return json_encode(array('AddX5List' => 'works'));
+    }
+}
