e71c890c36052444de061304a1e30ee31a61f11a
Nils Oesting <noesting@uni-osnabrueck.de>
2019-01-24 09:42:28 +0100

    edit get route for lists to accept argument course id

--- lib/Routes/Routemap.php
+++ lib/Routes/Routemap.php
@@ -7,7 +7,7 @@ class Routemap
     public static function registerAuthenticatedRoutes(\Slim\App $app)
     {
         $app->get('/list/{id}/show', Lists\GetX5List::class);
-        $app->get('/lists', Lists\GetX5Lists::class);
+        $app->get('/x5lists/{range_id}', Lists\GetX5Lists::class);
         $app->post('/list/add', Lists\AddX5List::class);
         $app->patch('/list/{id}/alter', Lists\AlterX5List::class);
         $app->get('/list/{id}/remove', Lists\RemoveX5List::class);
