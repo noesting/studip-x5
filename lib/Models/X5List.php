69aabb88d4befdd5aba533fb7c92ba24f1cfb1fd
Nils Oesting <noesting@uni-osnabrueck.de>
2019-01-24 09:42:05 +0100

    relate x5 list table to courses

--- lib/Models/X5List.php
+++ lib/Models/X5List.php
@@ -7,6 +7,10 @@ class X5List extends \SimpleORMap
     protected static function configure($config = array())
     {
         $config['db_table'] = 'x5_lists';
+        $config['belongs_to']['course'] = array(
+            'class_name' => 'Course',
+            'foreign_key' => 'range_id',
+        );
         parent::configure($config);
     }
 }
