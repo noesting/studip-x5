e81f92cc7959fdf83c3f0e5111c6fbc98930247e
Nils Oesting <noesting@uni-osnabrueck.de>
2019-01-24 09:43:32 +0100

    add route for x5lists for a course

--- lib/Routes/Lists/GetX5Lists.php
+++ lib/Routes/Lists/GetX5Lists.php
@@ -5,21 +5,21 @@ namespace X5\Routes\Lists;
 use Argonauts\JsonApiController;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use X5\Models\X5List;
 
 class GetX5Lists extends JsonApiController
 {
     public function __invoke(Request $request, Response $response, $args)
     {
-        // if (!$lists = X5List::find($args['id'])) {
-        //     throw new RecordNotFoundException();
-        // }
+        if (!$lists = X5List::findManyByRange_id($args['range_id'])) {
+            throw new RecordNotFoundException();
+        }
 
+        // TODO Authorization
         // if (1 == 2) {
         //     throw new AuthorizationFailedException();
         // }
 
-        // return $this->getContentResponse($lists);
-
-        return json_encode(array('GetX5Lists' => 'works'));
+        return $this->getContentResponse($lists);
     }
 }
