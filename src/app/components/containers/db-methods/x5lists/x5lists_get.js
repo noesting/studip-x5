226b3639a1e2141850a7b1340ff71afda7e7607e
Nils Oesting <noesting@uni-osnabrueck.de>
2019-02-05 06:49:27 +0100

    add list item edit and add methods to vue

--- src/app/components/containers/db-methods/x5lists/x5lists_get.js
+++ src/app/components/containers/db-methods/x5lists/x5lists_get.js
@@ -4,32 +4,62 @@ export const setInitialCustomLists = () => {
     return [{}];
 };
 
-export const setCustomListsFromDB = (customLists, dozentViewContainer) => {
+export const setCustomListsFromDB = (customLists, recommendations, dozentViewContainer) => {
     const headers = Connection.getHeaders();
     const rangeId = Connection.getRangeId();
 
     return dozentViewContainer.$http
         .get(Connection.REST_ENDPOINT + 'x5lists/' + rangeId, { headers })
-        .then(response => handleGetListsResponse(response, customLists));
+        .then(response => handleGetListsResponse(response, customLists, recommendations, dozentViewContainer));
 };
 
-const handleGetListsResponse = (response, customLists) => {
+const handleGetListsResponse = (response, customLists, recommendations, dozentViewContainer) => {
     if (response.ok) {
-        setCustomListsFromResponse(response, customLists);
+        setCustomListsFromResponse(response, customLists, recommendations, dozentViewContainer);
     }
 };
 
-const setCustomListsFromResponse = (response, customLists) => {
+const setCustomListsFromResponse = (response, customLists, recommendations, dozentViewContainer) => {
     for (let i = 0; i < response.body.data.length; i++) {
-        addListToArray(customLists, response.body.data[i]);
+        addListToArray(customLists, response.body.data[i], recommendations, dozentViewContainer);
     }
     customLists.shift();
 };
 
-const addListToArray = (customLists, data) => {
+const addListToArray = (customLists, data, recommendations, dozentViewContainer) => {
     customLists.push({
         id: data.id,
         title: data.attributes.title,
         list: []
     });
+
+    setItems(customLists[customLists.length - 1], recommendations, dozentViewContainer);
+};
+
+const setItems = (customList, recommendations, dozentViewContainer) => {
+    const headers = Connection.getHeaders();
+
+    return dozentViewContainer.$http
+        .get(Connection.REST_ENDPOINT + 'x5list/' + customList.id + '/items', { headers })
+        .then(response => {
+            if (response.ok) {
+                for (let i = 0; i < response.body.data.relationships['x5-items'].meta.length; i++) {
+                    const item = getItemFromRecommentdations(
+                        response.body.data.relationships['x5-items'].meta[i].id,
+                        recommendations
+                    );
+                    console.log('customList Item', item);
+                    customList.list.push(item);
+                }
+            }
+        });
+};
+
+const getItemFromRecommentdations = (itemId, recommendations) => {
+    for (let i = 0; i < recommendations.length; i++) {
+        if (recommendations[i].id === itemId) {
+            console.log('found');
+            return recommendations[i];
+        }
+    }
 };
