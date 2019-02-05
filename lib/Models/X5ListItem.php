b43b6319aa4e69aa8cad1cf63ca8f658e708806c
Nils Oesting <noesting@uni-osnabrueck.de>
2019-02-05 06:47:05 +0100

    link list item model to list

--- lib/Models/X5ListItem.php
+++ lib/Models/X5ListItem.php
@@ -7,6 +7,10 @@ class X5ListItem extends \SimpleORMap
     protected static function configure($config = array())
     {
         $config['db_table'] = 'x5_list_items';
+        $config['belongs_to']['x5list'] = array(
+            'class_name' => 'X5List',
+            'foreign_key' => 'list_id',
+        );
         parent::configure($config);
     }
 }
