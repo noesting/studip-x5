c0d8fcc2ac993679ed96f40ad35e1bd642aa43c7
Nils Oesting <noesting@uni-osnabrueck.de>
2019-02-05 06:48:54 +0100

    added item routes to routemap

--- lib/Routes/Routemap.php
+++ lib/Routes/Routemap.php
@@ -7,6 +7,8 @@ class Routemap
     public static function registerAuthenticatedRoutes(\Slim\App $app)
     {
         $app->get('/list/{id}/show', Lists\GetX5List::class);
+        $app->get('/x5list/{id}/items', Lists\GetItemsFromList::class);
+        $app->post('/x5list/{id}/items/add', Lists\AddItemsToList::class);
         $app->get('/x5lists/{range_id}', Lists\GetX5Lists::class);
         $app->post('/list/add', Lists\AddX5List::class);
         $app->patch('/list/{id}/alter', Lists\AlterX5List::class);
