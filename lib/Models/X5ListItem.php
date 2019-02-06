4daaed18b858fdb3ba149733d853d8478fd138ab
Nils Oesting <noesting@uni-osnabrueck.de>
2019-02-06 11:11:07 +0100

    fix belongs to for list items

--- lib/Models/X5ListItem.php
+++ lib/Models/X5ListItem.php
@@ -8,12 +8,13 @@ class X5ListItem extends \SimpleORMap
     {
         $config['db_table'] = 'x5_list_items';
         $config['belongs_to']['x5list'] = array(
-            'class_name' => 'X5List',
+            'class_name' => X5List::class,
             'foreign_key' => 'list_id',
         );
         $config['belongs_to']['x5item'] = array(
-            'class_name' => 'X5Item',
+            'class_name' => X5Item::class,
             'foreign_key' => 'item_id',
+            'assoc_func' => 'findByItem_id',
         );
         parent::configure($config);
     }
