28c349bc2203fcddecbe6e21497d33ed639cc25e
Nils Oesting <noesting@uni-osnabrueck.de>
2019-02-06 10:03:57 +0100

    use attributes from db, not enriched ones

--- lib/Schemas/X5Item.php
+++ lib/Schemas/X5Item.php
@@ -18,13 +18,17 @@ class X5Item extends SchemaProvider
     public function getAttributes($resource)
     {
         return [
-            'title' => $resource['title'],
-            'description' => $resource['description'],
-            'author' => $resource['author'],
-            'publishing-year' => $resource['publishingYear'],
-            'link' => $resource['link'],
-            'thumbs-ups' => (int) $resource['thumbsUps'],
-            'thumbnail' => $resource['thumbnail'],
+            // 'title' => $resource['title'],
+            // 'description' => $resource['description'],
+            // 'author' => $resource['author'],
+            // 'publishing-year' => $resource['publishingYear'],
+            // 'link' => $resource['link'],
+            // 'thumbs-ups' => (int) $resource['thumbsUps'],
+            // 'thumbnail' => $resource['thumbnail'],
+            'item_id' => $resource['item_id'],
+            'likes' => $resource['likes'],
+            'mkdate' => date('c', $resource['mkdate']),
+            'chdate' => date('c', $resource['chdate']),
         ];
     }
 
