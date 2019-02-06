42d61d758ef20009854897cdf0919ff5ff15da3c
Nils Oesting <noesting@uni-osnabrueck.de>
2019-02-06 08:45:40 +0100

    connect x5item model to right db table

--- lib/Models/X5Item.php
+++ lib/Models/X5Item.php
@@ -6,7 +6,7 @@ class X5Item extends \SimpleORMap
 {
     protected static function configure($config = array())
     {
-        $config['db_table'] = 'x5_list_items';
+        $config['db_table'] = 'x5_items';
         parent::configure($config);
     }
 }
