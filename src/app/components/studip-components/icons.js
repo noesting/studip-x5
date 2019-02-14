181dcf7bb43b49b506875618f5b1376faa20df65
Nils Oesting <noesting@uni-osnabrueck.de>
2019-02-14 09:33:21 +0100

    create icon index

---
+++ src/app/components/studip-components/icons.js
@@ -0,0 +1,33 @@
+import action from '../../../assets/icons/blue/action.svg';
+import arr_1down from '../../../assets/icons/blue/arr_1down.svg';
+import arr_1right from '../../../assets/icons/blue/arr_1right.svg';
+import arr_2right from '../../../assets/icons/blue/arr_2right.svg';
+import bookmark from '../../../assets/icons/blue/bookmark.svg';
+import decline from '../../../assets/icons/blue/decline.svg';
+import favorite from '../../../assets/icons/blue/favorite.svg';
+import file_text from '../../../assets/icons/blue/file-text.svg';
+import image from '../../../assets/icons/blue/image.svg';
+import menu_more from '../../../assets/icons/blue/menu-more.svg';
+import music from '../../../assets/icons/blue/music.svg';
+import search from '../../../assets/icons/blue/search.svg';
+import thumbs_up from '../../../assets/icons/blue/thumbs_up.svg';
+import trash from '../../../assets/icons/blue/trash.svg';
+import video2 from '../../../assets/icons/blue/video2.svg';
+
+export const studipIcons = {
+    action,
+    arr_1down,
+    arr_1right,
+    arr_2right,
+    bookmark,
+    decline,
+    favorite,
+    file_text,
+    image,
+    menu_more,
+    music,
+    search,
+    thumbs_up,
+    trash,
+    video2
+};
