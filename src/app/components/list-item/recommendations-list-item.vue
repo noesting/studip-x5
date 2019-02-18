f61648551acfe72f1819b7c1c9e625e427f8a6c5
Nils Oesting <noesting@uni-osnabrueck.de>
2019-02-18 14:47:47 +0100

    added basic conditional class styling

--- src/app/components/list-item/recommendations-list-item.vue
+++ src/app/components/list-item/recommendations-list-item.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="x5_list_item_container">
+    <div v-bind:class="[item.inList ? 'x5_list_item_container_in_list' : 'x5_list_item_container']">
         <div @click="openModal()">
             <ListItem v-bind:item="item" v-bind:key="item.id"></ListItem>
         </div>
@@ -57,6 +57,18 @@
         overflow: hidden;
         margin-top: 1em;
         background-color: #e7ebf1;
+
+        display: grid;
+        grid-template-columns: 95% 5%;
+    }
+
+    .x5_list_item_container_in_list {
+        width: 100%;
+        height: 4.5em;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        margin-top: 1em;
+        background-color: lightgrey !important;
 
         display: grid;
         grid-template-columns: 95% 5%;
