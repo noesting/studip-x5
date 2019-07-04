805292244c2329e3f5ad051d29fd39fcaef845ee
Florian Feyen <ff@flause.io>
2019-07-04 16:11:44 +0200

    add headers to removeListFromDB DELETE request

--- src/app/components/containers/db-methods/x5lists/x5lists_remove.js
+++ src/app/components/containers/db-methods/x5lists/x5lists_remove.js
@@ -2,8 +2,9 @@ import * as Connection from '../general';
 
 export const removeListFromDB = (customLists, deleteListIndex, dozentViewContainer) => {
     const idToDelete = customLists[deleteListIndex].id;
+    const headers = Connection.getHeaders();
 
-    dozentViewContainer.$http.delete(Connection.REST_ENDPOINT + 'x5-lists/' + idToDelete + '/delete').then(response => {
+    dozentViewContainer.$http.delete(Connection.REST_ENDPOINT + 'x5-lists/' + idToDelete + '/delete', { headers }).then(response => {
         if (response.ok) {
             customLists.splice(deleteListIndex, 1);
         }
