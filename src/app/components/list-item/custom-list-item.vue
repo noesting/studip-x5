03310dc05df64d89fe4a7da666f57b44149b07ce
Florian Feyen <ff@flause.io>
2019-07-29 14:35:30 +0200

    add reactive visual mark for items which are unread from the user in custom-list-item component

--- src/app/components/list-item/custom-list-item.vue
+++ src/app/components/list-item/custom-list-item.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="x5_list_item_container">
         <div @click="openModal()">
+            <div :class="{ x5_item_unread: !userRead }"></div>
             <ListItem v-bind:item="item" v-bind:key="item.id"></ListItem>
         </div>
         <!-- <div class="x5_list_item_comment" @click="action(item.id)">
@@ -35,7 +36,7 @@
         },
         data() {
             return {
-                itemMarkedAsRead: this.item.markedAsRead
+                userRead: this.item.userRead
             }
         },
         methods: {
@@ -67,6 +68,7 @@
             },
 
             markItemAsRead() {
+                this.userRead = true;
                 this.$emit('markItemAsRead', this.item);
             }
         }
@@ -109,6 +111,16 @@
         grid-column: 2;
 
         cursor: pointer;
+    }
+
+    .x5_item_unread {
+        position: absolute;
+        padding-bottom: 0.66em;
+        margin-top: -0.33em;
+        background-color: #1f3f70;
+        height: 4.5em;
+        width: 4px;
+        z-index: 10;
     }
 </style>
 
