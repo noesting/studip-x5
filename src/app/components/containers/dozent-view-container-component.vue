6b4f85679ef0327590632b38856b4a1d008d439e
Florian Feyen <ff@flause.io>
2019-08-19 16:15:11 +0200

    change var to let

--- src/app/components/containers/dozent-view-container-component.vue
+++ src/app/components/containers/dozent-view-container-component.vue
@@ -211,8 +211,8 @@
             },
 
             updateReadInCustomLists(itemId) {
-                for (var i = 0; i < this.customLists.length; i++) {
-                    for (var k = 0; k < this.customLists[i].list.length; k++) {
+                for (let i = 0; i < this.customLists.length; i++) {
+                    for (let k = 0; k < this.customLists[i].list.length; k++) {
                         if (this.customLists[i].list[k].id === itemId) {
                             this.customLists[i].list[k].userRead = true;
                         }
