bffefb56d7e867ef7c2b95b653644c912f3e6653
Florian Feyen <ff@flause.io>
2019-08-19 15:40:46 +0200

    prefilter response return to body.rec_materials and return recommendations

--- src/app/components/containers/x5api/recommendations-get.js
+++ src/app/components/containers/x5api/recommendations-get.js
@@ -2,16 +2,16 @@ import * as X5API from './x5api-config';
 
 export const getX5Recommendations = (courseMetadata, dozentViewContainer) => {
   // check for page and results count
-  let parameter = bundleTextParameter(courseMetadata);
-  console.log(parameter);
-  let recommendations = getRequestX5Api(parameter, dozentViewContainer);
-  console.log(recommendations);
-  // bundle response
-  // return response
+  let textParameter = bundleTextParameter(courseMetadata);
+  let recMaterial = getRequestX5Api(textParameter, dozentViewContainer).then(recommendations => {
+    return recommendations;
+  });
+  return recMaterial;
 };
 
 const bundleTextParameter = (courseMetadata) => {
-  let keysToInclude = ['title', 'subtitle', 'description'];
+  //let keysToInclude = ['title', 'subtitle', 'description'];
+  let keysToInclude = ['title', 'subtitle'];
   var textParameter = '';
 
   keysToInclude.forEach((key) => {
@@ -24,15 +24,12 @@ const bundleTextParameter = (courseMetadata) => {
 
 const getRequestX5Api = (parameter, dozentViewContainer) => {
   let headers = X5API.getHeaders;
-  
+
   return dozentViewContainer.$http
-        .get(X5API.SEARCH_ENDPOINT + parameter, { headers })
-        .then(response => handleX5GetResponse(response, dozentViewContainer));
+    .get(X5API.SEARCH_ENDPOINT + parameter, { headers })
+    .then(response => handleX5GetResponse(response, dozentViewContainer));
 };
 
 const handleX5GetResponse = (response, dozentViewContainer) => {
-  console.log(response);
-  return response;
+  return response.body.rec_materials;
 };
-  
-
