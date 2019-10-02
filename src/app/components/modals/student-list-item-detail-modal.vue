bceed586accf5a33a76ff2a84515469a1e5048d3
Florian Feyen <florian@feyen.net>
2019-10-02 19:48:31 +0200

    change style to widen comment field in students view modal

--- src/app/components/modals/student-list-item-detail-modal.vue
+++ src/app/components/modals/student-list-item-detail-modal.vue
@@ -116,9 +116,9 @@
     }
 
     .interaction-fields {
-        margin: 1em;
+        margin: 0.1em;
         display: grid;
-        grid-template-columns: 33% 33% 33%;
+        grid-template-columns: 66% 33%;
         grid-column-gap: 10px;
     }
 
