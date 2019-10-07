1d23f3c719c5e6ed469860fcebc0c70072c07ce9
Florian Feyen <florian@feyen.net>
2019-10-07 17:08:13 +0200

    add auth routes to X5ListItemUpdate class

--- lib/Routes/ListItems/X5ListItemUpdate.php
+++ lib/Routes/ListItems/X5ListItemUpdate.php
@@ -8,21 +8,23 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use X5\Models\X5ListItem;
 use X5\Schemas\X5ListItem as X5ListItemSchema;
+use JsonApi\Errors\RecordNotFoundException;
+use JsonApi\Errors\AuthorizationFailedException;
 
 class X5ListItemUpdate extends JsonApiController
 {
     use ValidationTrait;
     public function __invoke(Request $request, Response $response, $args)
     {
-        // if (!$x5listitrem = X5ListItem::find($args['id'])) {
         if (!$x5listitem = X5ListItem::findOneBySql('item_id = ? AND list_id = ?', [$args['item_id'], $args['list_id']])) {
             throw new RecordNotFoundException();
         }
 
-        // TODO Authorization
-        // if (1 == 2) {
-        //     throw new AuthorizationFailedException();
-        // }
+        global $perm;
+
+        if ($perm->have_studip_perm('dozent', $args['range_id'])) {
+            throw new AuthorizationFailedException();
+        };
 
         $result = $this->updateX5ListItem($request, $x5listitem);
 
