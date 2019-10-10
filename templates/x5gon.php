c7344bc9bc4eb38550afb1fda6a24a580c5a5009
Florian Feyen <florian@feyen.net>
2019-10-10 16:18:48 +0200

    add new folder and template file x5gon.php

---
+++ templates/x5gon.php
@@ -0,0 +1,4 @@
+<a href="https://platform.x5gon.org" target="_blank">
+  <img src="https://platform.x5gon.org/imgs/logo/x5gon_logo_dark.svg" alt="">
+</a>
+powered by <a href="https://platform.x5gon.org" target="_blank">X5GON</a>
