95ccfd0c13a5399fff02d3b567507fa5a4e72744
Florian Feyen <florian@feyen.net>
2019-10-10 17:03:56 +0200

    add links to issue tracker and github repo (GitHub) in sidebar

--- templates/x5gon.php
+++ templates/x5gon.php
@@ -1,15 +1,36 @@
-<p>powered by</p>
+<ul class="list">
+  <li class="list-item">
+    <a href="https://github.com/virtUOS/studip-x5/issues/" target="_blank">Einen Fehler melden (GitHub)</a>
+  </li>
+  <li class="list-item">
+    <a href="https://github.com/virtUOS/studip-x5/" target="_blank">Quellcode (GitHub)</a>
+  </li>
+</ul>
+<p class="italic">powered by</p>
 <a href="https://platform.x5gon.org" target="_blank">
   <img src="https://platform.x5gon.org/imgs/logo/x5gon_logo_light.svg" alt="powered by X5GON" class="logo">
 </a>
 
 <style>
-p {
+.italic {
   font-size: 1rem;
   font-style: italic;
 }
+
 .logo {
   width: 12rem;
   margin-left: 1rem;
+}
+
+.list {
+  padding: 0;
+  margin-bottom: 2rem;
+  display: inline-block;
+  text-align: left;
+}
+
+.list-item {
+  list-style: none;
+
 }
 </style>
