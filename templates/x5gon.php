6e1393bf2957d9aa948f593f6696893d8fcdd7f3
Florian Feyen <florian@feyen.net>
2019-10-10 16:46:28 +0200

    add styling to x5gon FlexiTemplate

--- templates/x5gon.php
+++ templates/x5gon.php
@@ -1,4 +1,15 @@
+<p>powered by</p>
 <a href="https://platform.x5gon.org" target="_blank">
-  <img src="https://platform.x5gon.org/imgs/logo/x5gon_logo_dark.svg" alt="">
+  <img src="https://platform.x5gon.org/imgs/logo/x5gon_logo_light.svg" alt="powered by X5GON" class="logo">
 </a>
-powered by <a href="https://platform.x5gon.org" target="_blank">X5GON</a>
+
+<style>
+p {
+  font-size: 1rem;
+  font-style: italic;
+}
+.logo {
+  width: 12rem;
+  margin-left: 1rem;
+}
+</style>
