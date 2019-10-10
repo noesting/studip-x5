796c3a31acb9191d8a349acc67740da6195ea8fa
Florian Feyen <florian@feyen.net>
2019-10-10 14:35:59 +0200

    add studip auth to X5ListShow

--- lib/Routes/Lists/X5ListShow.php
+++ lib/Routes/Lists/X5ListShow.php
@@ -10,13 +10,16 @@ class GetX5List extends JsonApiController
 {
     public function __invoke(Request $request, Response $response, $args)
     {
+        global $perm;
+        $x5lists = X5List::find($args['id']);
+
         // if (!$lists = X5List::find($args['id'])) {
         //     throw new RecordNotFoundException();
         // }
 
-        // if (1 == 2) {
-        //     throw new AuthorizationFailedException();
-        // }
+        if (!$perm->have_studip_perm('dozent', $x5lists->course->id)) {
+            throw new AuthorizationFailedException();
+        };    
 
         // return $this->getContentResponse($lists);
 
